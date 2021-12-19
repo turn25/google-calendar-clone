01b4dd50ae6db510d3e0b8038a5fe77832a31c3f
vuquangtuan123 <vuquangtuan12345@gmail.com>
2021-12-19 18:56:42 +0700

    fix: remove variant style from create event button

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -40,7 +40,6 @@ export default function Sidebar() {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            variants={variants}
             transition={{ duration: 0.2 }}
           >
             <CreateEventButtonIcon />
